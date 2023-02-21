e957317f99529b44130e0f03143b0b23bb82015d
Namfon <6410301005@cdti.ac.th>
2023-02-22 05:11:56 +0700

    new commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,4 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
-
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
